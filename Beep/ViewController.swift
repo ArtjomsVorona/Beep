4a109faf179b046bc682313fa4bd94acc4109398
Artjoms Vorona <artjomsvorona@gmail.com>
2019-11-08 15:50:03 +0200

    Initial version. Incl. AVPlayer observe. Button title changes Play/Stop.

--- Beep/ViewController.swift
+++ Beep/ViewController.swift
@@ -6,15 +6,36 @@
 //  Copyright © 2019 Artjoms Vorona. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var player = AVPlayer()
+    
+    @IBOutlet weak var playButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlay), name: .AVPlayerItemDidPlayToEndTime, object: nil)
+        playButton.tintColor = .systemBlue
     }
 
-
+    @IBAction func beepPressed(_ sender: UIButton) {
+        if player.timeControlStatus == .playing {
+            player.pause()
+            playButton.setTitle("Play", for: .normal)
+        } else {
+            let url = Bundle.main.url(forResource: "trumpet", withExtension: "mp3")
+            player = AVPlayer(url: url!)
+            player.play()
+            playButton.setTitle("STOP", for: .normal)
+        }
+    }
+    
+    @objc func playerDidFinishPlay() {
+        playButton.setTitle("Play", for: .normal)
+    }
+    
 }
 
